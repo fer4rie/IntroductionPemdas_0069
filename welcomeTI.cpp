a5270eefaeff5769408a429b583c8bd8b8a62b6e
Fira Puspita <firapuspita2106@gmail.com>
2026-02-24 16:50:59 +0700

    Add files via upload

---
+++ welcomeTI.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool cek = true;
+    char jk;
+    int angka = 10;
+    float tinggi = 100.6;
+    double hara = 200000.00;
+    string nama = "Fira";
+
+    jk= 'L';
+    cout << "Selamat Datang" << endl;
+    cout << "di TI UMY\n";
+    cout << "Yogyakarta\n";
+
+    nama = "Fira";
+    cout << " Nama saya : " << nama;
+
+
+}
