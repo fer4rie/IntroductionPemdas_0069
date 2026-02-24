0f04a96caab356e113a45cab3f9131409f42718d
fer4rie <firapuspita2106@gmail.com>
2026-02-24 16:21:59 +0700

    first commit

---
+++ latihan.cpp
@@ -0,0 +1,20 @@
+//deklarasi library
+#include <iostream>
+using namespace std;
+
+
+//progres utama
+int main()
+{
+    //deklarasi variabel umur
+    int umur;
+    //memberi nilai dari variabel
+    umur = 20;
+
+    //menampilkan isi string
+    cout << "miong" << endl;
+    //menampilkan isi integer
+    cout << 18 << endl;
+    //menampilkan isi dari variabel
+    cout << umur << endl;
+}
